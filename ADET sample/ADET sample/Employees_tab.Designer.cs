d1184e93069eeda88a922ced6374dd8bb7d56ed0
Lrotough <111031573+Lrotough@users.noreply.github.com>
2024-01-17 15:38:40 +0800

    added datagrid in employees tab

--- ADET sample/ADET sample/Employees_tab.Designer.cs
+++ ADET sample/ADET sample/Employees_tab.Designer.cs
@@ -28,8 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
             label1 = new Label();
             AddEventButton = new ADETADET_sample.RBButton();
+            Employees_List_Datagrid = new DataGridView();
+            employee_id = new DataGridViewTextBoxColumn();
+            firstname = new DataGridViewTextBoxColumn();
+            lastname = new DataGridViewTextBoxColumn();
+            status = new DataGridViewTextBoxColumn();
+            work = new DataGridViewTextBoxColumn();
+            rate = new DataGridViewTextBoxColumn();
+            edit_button = new DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)Employees_List_Datagrid).BeginInit();
             SuspendLayout();
             // 
             // label1
@@ -62,18 +73,82 @@
             AddEventButton.TextColor = Color.White;
             AddEventButton.UseVisualStyleBackColor = false;
             // 
+            // Employees_List_Datagrid
+            // 
+            Employees_List_Datagrid.AllowUserToAddRows = false;
+            Employees_List_Datagrid.AllowUserToDeleteRows = false;
+            Employees_List_Datagrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            Employees_List_Datagrid.BackgroundColor = Color.FromArgb(223, 232, 215);
+            Employees_List_Datagrid.BorderStyle = BorderStyle.None;
+            Employees_List_Datagrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            Employees_List_Datagrid.Columns.AddRange(new DataGridViewColumn[] { employee_id, firstname, lastname, status, work, rate, edit_button });
+            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = Color.FromArgb(223, 232, 215);
+            dataGridViewCellStyle4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle4.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = Color.FromArgb(46, 60, 24);
+            dataGridViewCellStyle4.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = DataGridViewTriState.False;
+            Employees_List_Datagrid.DefaultCellStyle = dataGridViewCellStyle4;
+            Employees_List_Datagrid.GridColor = Color.FromArgb(46, 60, 24);
+            Employees_List_Datagrid.Location = new Point(30, 56);
+            Employees_List_Datagrid.Name = "Employees_List_Datagrid";
+            Employees_List_Datagrid.Size = new Size(918, 498);
+            Employees_List_Datagrid.TabIndex = 11;
+            // 
+            // employee_id
+            // 
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = Color.FromArgb(223, 232, 215);
+            dataGridViewCellStyle3.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            employee_id.DefaultCellStyle = dataGridViewCellStyle3;
+            employee_id.HeaderText = "Employee ID";
+            employee_id.Name = "employee_id";
+            // 
+            // firstname
+            // 
+            firstname.HeaderText = "First Name";
+            firstname.Name = "firstname";
+            // 
+            // lastname
+            // 
+            lastname.HeaderText = "Last Name";
+            lastname.Name = "lastname";
+            // 
+            // status
+            // 
+            status.HeaderText = "Status";
+            status.Name = "status";
+            // 
+            // work
+            // 
+            work.HeaderText = "Work";
+            work.Name = "work";
+            // 
+            // rate
+            // 
+            rate.HeaderText = "Rate";
+            rate.Name = "rate";
+            // 
+            // edit_button
+            // 
+            edit_button.HeaderText = "Edit";
+            edit_button.Name = "edit_button";
+            // 
             // Employees_tab
             // 
             AutoScaleDimensions = new SizeF(8F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(223, 232, 215);
             ClientSize = new Size(981, 614);
+            Controls.Add(Employees_List_Datagrid);
             Controls.Add(AddEventButton);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Employees_tab";
             Text = "Employees_tab";
             Load += Employees_tab_Load;
+            ((System.ComponentModel.ISupportInitialize)Employees_List_Datagrid).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -82,5 +157,13 @@
 
         private Label label1;
         private ADETADET_sample.RBButton AddEventButton;
+        private DataGridView Employees_List_Datagrid;
+        private DataGridViewTextBoxColumn employee_id;
+        private DataGridViewTextBoxColumn firstname;
+        private DataGridViewTextBoxColumn lastname;
+        private DataGridViewTextBoxColumn status;
+        private DataGridViewTextBoxColumn work;
+        private DataGridViewTextBoxColumn rate;
+        private DataGridViewButtonColumn edit_button;
     }
 }
